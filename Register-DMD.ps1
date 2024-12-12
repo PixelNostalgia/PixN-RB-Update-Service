8064ecb55295c8e4793c79be0be12b59db47f009
Rapster-UK <rapsters.tech.hangout@gmail.com>
2024-12-12 13:51:59 +0000

    Create Register-DMD.ps1

---
+++ Register-DMD.ps1
@@ -0,0 +1,5 @@
+# Define the path where gamelist.xml is located, two levels up from the current script, inside system\es-menu
+
+..\..\emulators\vpinball\VPinMAME>FlexDMDUI.exe /register .\VPinMAME
+
+..\..\emulators\vpinball\VPinMAME>FlexDMDUI.exe /register-udmd .\VPinMAME
