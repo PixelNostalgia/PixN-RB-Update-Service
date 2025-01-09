e239ea80bf133c4e26c91335909e5e2c4fec2d49
Rapster-UK <rapsters.tech.hangout@gmail.com>
2025-01-09 17:36:06 +0000

    Update PixN-RB-Update-Service-Payload.cmd

--- PixN-RB-Update-Service-Payload.cmd
+++ PixN-RB-Update-Service-Payload.cmd
@@ -15,12 +15,15 @@ ECHO Wscript.Sleep 900                                    >>"%TempVBSFile%"
 
 CSCRIPT //nologo "%TempVBSFile%"
 
-rem Read from ASCII.txt and visualize ASCII art
+REM Function to handle errors with a pause
+set "handle_error=timeout /t 4 >nul"
+
+REM Read from ASCII.txt and visualize ASCII art
 type ASCII.txt
 
 echo .
 echo Pixel Nostalgia updater running...
-echo Version 1.22
+echo Version 1.23
 echo .
 ping -n 2 127.0.0.1 > nul
 
@@ -30,8 +33,15 @@ echo Updating system config files...
 echo .
 ping -n 2 127.0.0.1 > nul
 ..\..\emulators\pixn\PortableGit\cmd\git clone https://github.com/RGS-MBU/emulationstation.git
+if %ERRORLEVEL% neq 0 (
+    echo Download Failed! - Skipping...
+    %handle_error%
+) else (
+    echo Download Completed Successfully...
+)
 move /Y ".\emulationstation\.emulationstation\*.cfg" ..\..\emulationstation\.emulationstation\
 rmdir /S /Q ".\emulationstation"
+:skip
 ping -n 2 127.0.0.1 > nul
 echo .
 
@@ -40,32 +50,68 @@ echo .
 echo Updating the PixN Custom Collections...
 echo .
 ping -n 2 127.0.0.1 > nul
-rmdir /S /Q ".\PixN-Collections"
+rmdir /S /Q ".\PixN-Collections" >nul 2>&1
 md "..\..\emulationstation\.emulationstation\collections" >nul 2>&1
 ..\..\emulators\pixn\PortableGit\cmd\git clone https://github.com/PixelNostalgia/PixN-Collections.git
+if %ERRORLEVEL% neq 0 (
+    echo Download Failed! - Skipping...
+    %handle_error%
+) else (
+    echo Download Completed Successfully...
+)
 move /Y ".\PixN-Collections\*.cfg" ..\..\emulationstation\.emulationstation\collections\
 rmdir /S /Q ".\PixN-Collections"
+:skip
 ping -n 2 127.0.0.1 > nul
 echo .
 
 REM This section pulls down the latest es-checkversion script...
 echo .
-echo Updating es-checkversion script...
+echo Updating es-checkversion script if required...
 echo .
 ping -n 2 127.0.0.1 > nul
+IF EXIST "es-checkversion-v1" goto SKIP
 wget https://raw.githubusercontent.com/PixelNostalgia/PixN-RB-Update-Service/main/es-checkversion-v6.4.cmd -O es-checkversion.cmd
+if %ERRORLEVEL% neq 0 (
+    echo Download Failed! - Skipping...
+    %handle_error%
+	goto SKIP
+) else (
+    echo Download Completed Successfully...
+)
 move /Y "es-checkversion.cmd" ..\..\emulationstation\
-ping -n 2 127.0.0.1 > nul
+ping -n 1 127.0.0.1 > nul
+echo es-checkversion-v1 > es-checkversion-v1
+:skip
 echo .
+ping -n 2 127.0.0.1 > nul
 
 REM This section restores the PixN Update Service artwork...
+echo Checking if the PixN Update Service artwork needs restoring...
+echo .
 wget https://raw.githubusercontent.com/PixelNostalgia/PixN-RB-Update-Service/main/Add-PixNService.ps1 -O Add-PixNService.ps1
+if %ERRORLEVEL% neq 0 (
+    echo Download Failed! - Skipping...
+    %handle_error%
+	goto SKIP
+) else (
+    echo Download Completed Successfully...
+)
 ping -n 2 127.0.0.1 > nul
 powershell -ExecutionPolicy Bypass -File "Add-PixNService.ps1"
 ping -n 2 127.0.0.1 > nul
+:skip
 del /Q "pixn-rb-update-service-logo.png"
 wget https://raw.githubusercontent.com/PixelNostalgia/PixN-RB-Update-Service/main/pixn-rb-update-service-logo.png -O pixn-rb-update-service-logo.png
+if %ERRORLEVEL% neq 0 (
+    echo Download Failed! - Skipping...
+    %handle_error%
+	got SKIP
+) else (
+    echo Download Completed Successfully...
+)
 move /Y "pixn-rb-update-service-logo.png" ..\..\system\es_menu\media\
+:skip
 ping -n 1 127.0.0.1 > nul
 echo .
 
@@ -83,16 +129,37 @@ REM Clean up PixN from System Wheel
 rmdir /S /Q "..\..\roms\pixn" >nul 2>&1
 
 REM This section applies the PinballFX and Piball M Fix...
-echo PinballFX and Piball M Fix...
+echo Applying PinballFX and Piball M Fix if required...
 echo .
 ping -n 2 127.0.0.1 > nul
+IF EXIST "pinballfx-v1" goto SKIP
 wget https://raw.githubusercontent.com/PixelNostalgia/PixN-RB-Update-Service/main/Pin-Lic.7z -O Pin-Lic.7z
+if %ERRORLEVEL% neq 0 (
+    echo Download Failed! - Skipping...
+    %handle_error%
+	goto SKIP
+) else (
+    echo Download Completed Successfully...
+)
 wget https://raw.githubusercontent.com/PixelNostalgia/PixN-RB-Update-Service/main/7z.exe -O 7z.exe
+if %ERRORLEVEL% neq 0 (
+    echo Download Failed! - Skipping...
+    %handle_error%
+	goto SKIP
+) else (
+    echo Download Completed Successfully...
+)
 wget https://raw.githubusercontent.com/PixelNostalgia/PixN-RB-Update-Service/main/7z.dll -O 7z.dll
+if %ERRORLEVEL% neq 0 (
+    echo Download Failed! - Skipping...
+    %handle_error%
+	goto SKIP
+) else (
+    echo Download Completed Successfully...
+)
 ping -n 2 127.0.0.1 > nul
-REM @echo on
 7z x Pin-Lic.7z -aoa -p22446688 -o.\
-echo .
+
 md "%localappdata%\PinballFX"
 md "%localappdata%\PinballM"
 
@@ -106,14 +173,25 @@ robocopy "PinballM\Saved\SaveGames" "%localappdata%\PinballM\Saved\SaveGames" /m
 rmdir /S /Q "PinballFX"
 rmdir /S /Q "PinballM"
 del /Q Pin-Lic.7z
+ping -n 1 127.0.0.1 > nul
+echo pinballfx-v1 > pinballfx-v1
+:skip
+echo .
 ping -n 2 127.0.0.1 > nul
 
 REM This section applies Zaccaria Pinball config...
-REM echo Zaccaria Pinball Config...
+echo Applying Zaccaria Pinball Config if required...
 echo .
 ping -n 2 127.0.0.1 > nul
 IF EXIST "ZP-v1" goto SKIP
 wget https://raw.githubusercontent.com/PixelNostalgia/PixN-RB-Update-Service/main/ZP.7z -O ZP.7z
+if %ERRORLEVEL% neq 0 (
+    echo Download Failed! - Skipping...
+    %handle_error%
+	goto SKIP
+) else (
+    echo Download Completed Successfully...
+)
 ping -n 2 127.0.0.1 > nul
 7z x ZP.7z -aoa -p22446688 -o.\
 echo .
@@ -126,24 +204,35 @@ echo n | copy /-y "Zaccaria_Pinball" "%PixN-MyDocs%\Zaccaria_Pinball"
 ping -n 2 127.0.0.1 > nul
 rmdir /S /Q "Zaccaria_Pinball"
 del /Q ZP.7z
-echo .
-ping -n 1 127.0.0.1 > nul
 echo ZP-v1 > ZP-v1
 :skip
+echo .
+ping -n 2 127.0.0.1 > nul
 
 REM This section adds OpenAL32.dll if required...
 echo Checking if OpenAL32.dll is required...
 echo .
 ping -n 2 127.0.0.1 > nul
+IF EXIST "OpenAL32.dll-v1" goto SKIP
 IF NOT EXIST ..\..\roms\zaccariapinball\ZaccariaPinball.pc\ goto SKIP
 IF EXIST ..\..\roms\zaccariapinball\ZaccariaPinball.pc\OpenAL32.dll goto SKIP
 
 wget https://raw.githubusercontent.com/PixelNostalgia/PixN-RB-Update-Service/main/OpenAL32.dll -O OpenAL32.dll
+if %ERRORLEVEL% neq 0 (
+    echo Download Failed! - Skipping...
+    %handle_error%
+	goto SKIP
+) else (
+    echo Download Completed Successfully...
+)
 ping -n 2 127.0.0.1 > nul
 copy OpenAL32.dll ..\..\roms\zaccariapinball\ZaccariaPinball.pc\
 echo .
 del /Q OpenAL32.dll
+echo OpenAL32.dll-v1 > OpenAL32.dll-v1
 :skip
+echo .
+ping -n 2 127.0.0.1 > nul
 
 REM This section checks for updated Radio stations...
 echo Checking for updated Radio Stations...
@@ -151,8 +240,29 @@ echo .
 ping -n 2 127.0.0.1 > nul
 IF EXIST "Radio-v2" goto SKIP
 wget https://raw.githubusercontent.com/PixelNostalgia/PixN-RB-Update-Service/main/radio.7z.001 -O radio.7z.001
+if %ERRORLEVEL% neq 0 (
+    echo Download Failed! - Skipping...
+    %handle_error%
+	goto SKIP
+) else (
+    echo Download Completed Successfully...
+)
 wget https://raw.githubusercontent.com/PixelNostalgia/PixN-RB-Update-Service/main/radio.7z.002 -O radio.7z.002
+if %ERRORLEVEL% neq 0 (
+    echo Download Failed! - Skipping...
+    %handle_error%
+	goto SKIP
+) else (
+    echo Download Completed Successfully...
+)
 wget https://raw.githubusercontent.com/PixelNostalgia/PixN-RB-Update-Service/main/radio.7z.003 -O radio.7z.003
+if %ERRORLEVEL% neq 0 (
+    echo Download Failed! - Skipping...
+    %handle_error%
+	goto SKIP
+) else (
+    echo Download Completed Successfully...
+)
 ping -n 2 127.0.0.1 > nul
 7z x radio.7z.001 -aoa -p22446688 -o..\..\roms\radio\
 echo .
@@ -162,12 +272,22 @@ del /Q radio.7z.003
 
 echo Radio-v2 > Radio-v2
 :skip
-
-REM This sections fixes the version of the Archmendes BIOS files...
-echo Downloading updated Archmendes BIOS files...
 echo .
 ping -n 2 127.0.0.1 > nul
+
+REM This sections fixes the version of the Archmendes BIOS files...
+echo Downloading updated Archmendes BIOS files if required...
+echo .
+ping -n 2 127.0.0.1 > nul
+IF EXIST "Archmendes-BIOS-v1" goto SKIP
 wget https://raw.githubusercontent.com/PixelNostalgia/PixN-RB-Update-Service/main/arch-b.7z -O arch-b.7z
+if %ERRORLEVEL% neq 0 (
+    echo Download Failed! - Skipping...
+    %handle_error%
+	goto SKIP
+) else (
+    echo Download Completed Successfully...
+)
 ping -n 2 127.0.0.1 > nul
 echo .
 7z x arch-b.7z -aoa -p22446688 -o.\
@@ -177,17 +297,24 @@ move /Y "aa310.zip" ..\..\bios\
 move /Y "archimedes_keyboard.zip" ..\..\bios\
 ping -n 2 127.0.0.1 > nul
 del /Q arch-b.7z
+echo Archmendes-BIOS-v1 > Archmendes-BIOS-v1
+:skip
 echo .
 ping -n 2 127.0.0.1 > nul
 
 REM This section adds the Skylanders files to the Dolphin Emulator...
-echo Adding Skylanders files to the Dolphin Emulator...
+echo Adding Skylanders files to the Dolphin Emulator if required...
 echo .
 ping -n 2 127.0.0.1 > nul
-
 IF EXIST "Sky-v1" goto SKIP
-
 wget https://raw.githubusercontent.com/PixelNostalgia/PixN-RB-Update-Service/main/Sky.7z -O Sky.7z
+if %ERRORLEVEL% neq 0 (
+    echo Download Failed! - Skipping...
+    %handle_error%
+	goto SKIP
+) else (
+    echo Download Completed Successfully...
+)
 ping -n 2 127.0.0.1 > nul
 echo .
 7z x Sky.7z -aoa -p22446688 -o.\
@@ -213,14 +340,77 @@ ping -n 2 127.0.0.1 > nul
 IF EXIST "Hypseus-v1" goto SKIP
 
 wget https://raw.githubusercontent.com/PixelNostalgia/PixN-RB-Update-Service/main/hypseus.7z.001 -O hypseus.7z.001
+if %ERRORLEVEL% neq 0 (
+    echo Download Failed! - Skipping...
+    %handle_error%
+	goto SKIP
+) else (
+    echo Download Completed Successfully...
+)
 wget https://raw.githubusercontent.com/PixelNostalgia/PixN-RB-Update-Service/main/hypseus.7z.002 -O hypseus.7z.002
+if %ERRORLEVEL% neq 0 (
+    echo Download Failed! - Skipping...
+    %handle_error%
+	goto SKIP
+) else (
+    echo Download Completed Successfully...
+)
 wget https://raw.githubusercontent.com/PixelNostalgia/PixN-RB-Update-Service/main/hypseus.7z.003 -O hypseus.7z.003
+if %ERRORLEVEL% neq 0 (
+    echo Download Failed! - Skipping...
+    %handle_error%
+	goto SKIP
+) else (
+    echo Download Completed Successfully...
+)
 wget https://raw.githubusercontent.com/PixelNostalgia/PixN-RB-Update-Service/main/hypseus.7z.004 -O hypseus.7z.004
+if %ERRORLEVEL% neq 0 (
+    echo Download Failed! - Skipping...
+    %handle_error%
+	goto SKIP
+) else (
+    echo Download Completed Successfully...
+)
 wget https://raw.githubusercontent.com/PixelNostalgia/PixN-RB-Update-Service/main/hypseus.7z.005 -O hypseus.7z.005
+if %ERRORLEVEL% neq 0 (
+    echo Download Failed! - Skipping...
+    %handle_error%
+	goto SKIP
+) else (
+    echo Download Completed Successfully...
+)
 wget https://raw.githubusercontent.com/PixelNostalgia/PixN-RB-Update-Service/main/hypseus.7z.006 -O hypseus.7z.006
+if %ERRORLEVEL% neq 0 (
+    echo Download Failed! - Skipping...
+    %handle_error%
+	goto SKIP
+) else (
+    echo Download Completed Successfully...
+)
 wget https://raw.githubusercontent.com/PixelNostalgia/PixN-RB-Update-Service/main/hypseus.7z.007 -O hypseus.7z.007
+if %ERRORLEVEL% neq 0 (
+    echo Download Failed! - Skipping...
+    %handle_error%
+	goto SKIP
+) else (
+    echo Download Completed Successfully...
+)
 wget https://raw.githubusercontent.com/PixelNostalgia/PixN-RB-Update-Service/main/hypseus.7z.008 -O hypseus.7z.008
+if %ERRORLEVEL% neq 0 (
+    echo Download Failed! - Skipping...
+    %handle_error%
+	goto SKIP
+) else (
+    echo Download Completed Successfully...
+)
 wget https://raw.githubusercontent.com/PixelNostalgia/PixN-RB-Update-Service/main/hypseus.7z.009 -O hypseus.7z.009
+if %ERRORLEVEL% neq 0 (
+    echo Download Failed! - Skipping...
+    %handle_error%
+	goto SKIP
+) else (
+    echo Download Completed Successfully...
+)
 ping -n 2 127.0.0.1 > nul
 echo .
 7z x hypseus.7z.001 -aoa -p22446688 -o.\
@@ -243,6 +433,7 @@ rmdir /S /Q hypseus >nul 2>&1
 echo Hypseus-v1 > Hypseus-v1
 :skip
 echo .
+ping -n 2 127.0.0.1 > nul
 
 REM This section checks for the updated 3dSen Emulator...
 echo Checking for the updated 3dSen Emulator...
@@ -252,9 +443,37 @@ ping -n 2 127.0.0.1 > nul
 IF EXIST "3dSen-v1" goto SKIP
 
 wget https://raw.githubusercontent.com/PixelNostalgia/PixN-RB-Update-Service/main/3d-N.7z.001 -O 3d-N.7z.001
+if %ERRORLEVEL% neq 0 (
+    echo Download Failed! - Skipping...
+    %handle_error%
+	goto SKIP
+) else (
+    echo Download Completed Successfully...
+)
 wget https://raw.githubusercontent.com/PixelNostalgia/PixN-RB-Update-Service/main/3d-N.7z.002 -O 3d-N.7z.002
+if %ERRORLEVEL% neq 0 (
+    echo Download Failed! - Skipping...
+    %handle_error%
+	goto SKIP
+) else (
+    echo Download Completed Successfully...
+)
 wget https://raw.githubusercontent.com/PixelNostalgia/PixN-RB-Update-Service/main/3d-N.7z.003 -O 3d-N.7z.003
+if %ERRORLEVEL% neq 0 (
+    echo Download Failed! - Skipping...
+    %handle_error%
+	goto SKIP
+) else (
+    echo Download Completed Successfully...
+)
 wget https://raw.githubusercontent.com/PixelNostalgia/PixN-RB-Update-Service/main/3d-N.7z.004 -O 3d-N.7z.004
+if %ERRORLEVEL% neq 0 (
+    echo Download Failed! - Skipping...
+    %handle_error%
+	goto SKIP
+) else (
+    echo Download Completed Successfully...
+)
 ping -n 2 127.0.0.1 > nul
 echo .
 7z x 3d-N.7z.001 -aoa -p22446688 -o.\
@@ -278,11 +497,16 @@ REM This section checks for the updated TeknoParrot Emulator...
 echo Checking for the updated TeknoParrot Emulator...
 echo .
 ping -n 2 127.0.0.1 > nul
-
 IF EXIST "TeknoParrot-v3" goto SKIP
-
 del /Q xUH7WGQ4*.* >nul 2>&1
 wget https://pixeldrain.com/api/filesystem/xUH7WGQ4
+if %ERRORLEVEL% neq 0 (
+    echo Download Failed! - Skipping...
+    %handle_error%
+	goto SKIP
+) else (
+    echo Download Completed Successfully...
+)
 ren xUH7WGQ4 teknoparrot_jan2025.7z
 ping -n 2 127.0.0.1 > nul
 echo .
@@ -304,11 +528,16 @@ REM This section checks for the updated Virtual Pinball Emulator...
 echo Checking for the updated Virtual Pinball Emulator...
 echo .
 ping -n 2 127.0.0.1 > nul
-
 IF EXIST "VPinball-v2" goto SKIP
-
 del /Q tt4eBrw2*.* >nul 2>&1
 wget https://pixeldrain.com/api/filesystem/tt4eBrw2
+if %ERRORLEVEL% neq 0 (
+    echo Download Failed! - Skipping...
+    %handle_error%
+	goto SKIP
+) else (
+    echo Download Completed Successfully...
+)
 ren tt4eBrw2 vpinball_jan2025.7z
 ping -n 2 127.0.0.1 > nul
 echo .
@@ -330,11 +559,16 @@ REM This section checks for the updated Switch Emulators...
 echo Checking for the updated Switch Emulators...
 echo .
 ping -n 2 127.0.0.1 > nul
-
 IF EXIST "Switch-v1" goto SKIP
-
 del /Q sBugmhpq*.* >nul 2>&1
 wget https://pixeldrain.com/api/filesystem/sBugmhpq
+if %ERRORLEVEL% neq 0 (
+    echo Download Failed! - Skipping...
+    %handle_error%
+	goto SKIP
+) else (
+    echo Download Completed Successfully...
+)
 ren sBugmhpq switch_dec2024.7z
 ping -n 2 127.0.0.1 > nul
 echo .
@@ -387,8 +621,8 @@ REM echo .
 REM This section downloads a tiny file so we can see how many people are using the Update Service...
 del /Q NYcXqrtb*.* >nul 2>&1
 del /Q PixN-Stats >nul 2>&1
-wget https://pixeldrain.com/api/filesystem/NYcXqrtb
-ren NYcXqrtb PixN-Stats
+wget https://pixeldrain.com/api/filesystem/NYcXqrtb >nul 2>&1
+ren NYcXqrtb PixN-Stats >nul 2>&1
 ping -n 2 127.0.0.1 > nul
 
 REM This section enables HD texture packs for the NES HD system...
@@ -401,7 +635,7 @@ REM Set variable for the file path (relative to the script's location)
 set "filePath=..\..\emulationstation\.emulationstation\es_settings.cfg"
 
 REM Backup the original file
-copy "%filePath%" "%filePath%.bak"
+copy "%filePath%" "%filePath%.bak" >nul 2>&1
 
 REM Execute PowerShell command in Bypass mode
 powershell -ExecutionPolicy Bypass -Command ^
