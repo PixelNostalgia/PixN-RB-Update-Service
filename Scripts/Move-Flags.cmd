b6fa1f486e8bf58ed68e77e584a4f2fa25ec7500
Team Pixel Nostalgia <team.pixel.nostalgia@gmail.com>
2026-04-24 19:39:39 +0100

    Update Move-Flags.cmd

--- Scripts/Move-Flags.cmd
+++ Scripts/Move-Flags.cmd
@@ -1,79 +1,86 @@
-move /Y 3dSen-v1 .\Flags
-move /Y Aquarius-BIOS-v1 .\Flags
-move /Y Archimedes-BIOS-v1 .\Flags
-move /Y Archmendes-BIOS-v1 .\Flags
-move /Y Astrocade-BIOS-v1 .\Flags
-move /Y cgenius-emu-v1 .\Flags
-move /Y citron-emu-v1 .\Flags
-move /Y clonehero-emu-v1 .\Flags
-move /Y dolphin-bios-v1 .\Flags
-move /Y DOSbox-Pure-MIDI-v1 .\Flags
-move /Y Dragon-BIOS-v1 .\Flags
-move /Y Dragon-BIOS-v2 .\Flags
-move /Y ECWolf-BIOS-v1 .\Flags
-move /Y eka-emu-v1 .\Flags
-move /Y eka-v1 .\Flags
-move /Y Electron-BIOS-v1 .\Flags
-move /Y es-checkversion-v1 .\Flags
-move /Y hotd2-bios-files-v1 .\Flags
-move /Y Hypseus-v1 .\Flags
-move /Y konami-LCD-artwork-v1 .\Flags
-move /Y kronos-emu-v1 .\Flags
-move /Y lime3ds-emu-v1 .\Flags
-move /Y magicengine-emu-v1 .\Flags
-move /Y mandarine-emu-v1 .\Flags
-move /Y naomi-bios-files-v1 .\Flags
-move /Y openjazz-emu-v1 .\Flags
-move /Y paprium-core-v1 .\Flags
-move /Y pdark-emu-v1 .\Flags
-move /Y pinballfx-v1 .\Flags
-move /Y PixN-Radio-v1 .\Flags
-move /Y PixN-Radio-v2 .\Flags
-move /Y PixN-Radio-v3 .\Flags
-move /Y PixN-Radio-v4 .\Flags
-move /Y PixN-Radio-v5 .\Flags
-move /Y PixN-Stats .\Flags
-move /Y pixn-us-check-v1 .\Flags
-move /Y PS3-m3u-update-v1 .\Flags
-move /Y PS3-M3U-Rename-v1 .\Flags
-move /Y Radio-v2 .\Flags
-move /Y Radio-v3 .\Flags
-move /Y Radio-v4 .\Flags
-move /Y rb-7.3+bios_11-08-2025 .\Flags
-move /Y rb-7.3+emulators_11-08-2025 .\Flags
-move /Y rclone-v1 .\Flags
-move /Y rclone-v2 .\Flags
-move /Y rclone-v3 .\Flags
-move /Y retroarch-emu-v1 .\Flags
-move /Y ryujinx-emu-v1 .\Flags
-move /Y ryujinx-emu-v2 .\Flags
-move /Y Ryujinx-FW-v21.0.0 .\Flags
-move /Y Set-PixN-Splash-v1 .\Flags
-move /Y Set-PixN-Splash-v2 .\Flags
-move /Y shadps4-emu-v1 .\Flags
-move /Y shadps4-emu-v2 .\Flags
-move /Y Sky-v1 .\Flags
-move /Y solarus-emu-v1 .\Flags
-move /Y Supracan-BIOS-v1 .\Flags
-move /Y supracan-umc6650-v1 .\Flags
-move /Y Switch-Eden-v1 .\Flags
-move /Y Switch-v1 .\Flags
-move /Y Switch-v2 .\Flags
-move /Y TeknoParrot-v1 .\Flags
-move /Y TeknoParrot-v2 .\Flags
-move /Y TeknoParrot-v3 .\Flags
-move /Y TeknoParrot-v4 .\Flags
-move /Y TriForce-Config-v1 .\Flags
-move /Y VPinball-v1 .\Flags
-move /Y VPinball-v2 .\Flags
-move /Y win98-bios-v1 .\Flags
-move /Y win98-bios-v2 .\Flags
-move /Y Xash3D-FWGS-v1 .\Flags
-move /Y xenia-canary-emu-v1 .\Flags
-move /Y xenia-canary-emu-v2 .\Flags
-move /Y xenia-emu-v1 .\Flags
-move /Y xenia-emu-v2 .\Flags
-move /Y xenia-manager-emu-v1 .\Flags
-move /Y yabasanshiro-emu-v1 .\Flags
-move /Y ZP-v1 .\Flags
+@echo off
+REM Text color code for Light Green is A
+set "colorCode=A"
+color %colorCode%
+echo.
+echo Cleaning up old files...
+echo.
+move /Y 3dSen-v1 .\Flags >nul 2>&1
+move /Y Aquarius-BIOS-v1 .\Flags >nul 2>&1
+move /Y Archimedes-BIOS-v1 .\Flags >nul 2>&1
+move /Y Archmendes-BIOS-v1 .\Flags >nul 2>&1
+move /Y Astrocade-BIOS-v1 .\Flags >nul 2>&1
+move /Y cgenius-emu-v1 .\Flags >nul 2>&1
+move /Y citron-emu-v1 .\Flags >nul 2>&1
+move /Y clonehero-emu-v1 .\Flags >nul 2>&1
+move /Y dolphin-bios-v1 .\Flags >nul 2>&1
+move /Y DOSbox-Pure-MIDI-v1 .\Flags >nul 2>&1
+move /Y Dragon-BIOS-v1 .\Flags >nul 2>&1
+move /Y Dragon-BIOS-v2 .\Flags >nul 2>&1
+move /Y ECWolf-BIOS-v1 .\Flags >nul 2>&1
+move /Y eka-emu-v1 .\Flags >nul 2>&1
+move /Y eka-v1 .\Flags >nul 2>&1
+move /Y Electron-BIOS-v1 .\Flags >nul 2>&1
+move /Y es-checkversion-v1 .\Flags >nul 2>&1
+move /Y hotd2-bios-files-v1 .\Flags >nul 2>&1
+move /Y Hypseus-v1 .\Flags >nul 2>&1
+move /Y konami-LCD-artwork-v1 .\Flags >nul 2>&1
+move /Y kronos-emu-v1 .\Flags >nul 2>&1
+move /Y lime3ds-emu-v1 .\Flags >nul 2>&1
+move /Y magicengine-emu-v1 .\Flags >nul 2>&1
+move /Y mandarine-emu-v1 .\Flags >nul 2>&1
+move /Y naomi-bios-files-v1 .\Flags >nul 2>&1
+move /Y openjazz-emu-v1 .\Flags >nul 2>&1
+move /Y paprium-core-v1 .\Flags >nul 2>&1
+move /Y pdark-emu-v1 .\Flags >nul 2>&1
+move /Y pinballfx-v1 .\Flags >nul 2>&1
+move /Y PixN-Radio-v1 .\Flags >nul 2>&1
+move /Y PixN-Radio-v2 .\Flags >nul 2>&1
+move /Y PixN-Radio-v3 .\Flags >nul 2>&1
+move /Y PixN-Radio-v4 .\Flags >nul 2>&1
+move /Y PixN-Radio-v5 .\Flags >nul 2>&1
+move /Y PixN-Stats .\Flags >nul 2>&1
+move /Y pixn-us-check-v1 .\Flags >nul 2>&1
+move /Y PS3-m3u-update-v1 .\Flags >nul 2>&1
+move /Y PS3-M3U-Rename-v1 .\Flags >nul 2>&1
+move /Y Radio-v2 .\Flags >nul 2>&1
+move /Y Radio-v3 .\Flags >nul 2>&1
+move /Y Radio-v4 .\Flags >nul 2>&1
+move /Y rb-7.3+bios_11-08-2025 .\Flags >nul 2>&1
+move /Y rb-7.3+emulators_11-08-2025 .\Flags >nul 2>&1
+move /Y rclone-v1 .\Flags >nul 2>&1
+move /Y rclone-v2 .\Flags >nul 2>&1
+move /Y rclone-v3 .\Flags >nul 2>&1
+move /Y retroarch-emu-v1 .\Flags >nul 2>&1
+move /Y ryujinx-emu-v1 .\Flags >nul 2>&1
+move /Y ryujinx-emu-v2 .\Flags >nul 2>&1
+move /Y Ryujinx-FW-v21.0.0 .\Flags >nul 2>&1
+move /Y Set-PixN-Splash-v1 .\Flags >nul 2>&1
+move /Y Set-PixN-Splash-v2 .\Flags >nul 2>&1
+move /Y shadps4-emu-v1 .\Flags >nul 2>&1
+move /Y shadps4-emu-v2 .\Flags >nul 2>&1
+move /Y Sky-v1 .\Flags >nul 2>&1
+move /Y solarus-emu-v1 .\Flags >nul 2>&1
+move /Y Supracan-BIOS-v1 .\Flags >nul 2>&1
+move /Y supracan-umc6650-v1 .\Flags >nul 2>&1
+move /Y Switch-Eden-v1 .\Flags >nul 2>&1
+move /Y Switch-v1 .\Flags >nul 2>&1
+move /Y Switch-v2 .\Flags >nul 2>&1
+move /Y TeknoParrot-v1 .\Flags >nul 2>&1
+move /Y TeknoParrot-v2 .\Flags >nul 2>&1
+move /Y TeknoParrot-v3 .\Flags >nul 2>&1
+move /Y TeknoParrot-v4 .\Flags >nul 2>&1
+move /Y TriForce-Config-v1 .\Flags >nul 2>&1
+move /Y VPinball-v1 .\Flags >nul 2>&1
+move /Y VPinball-v2 .\Flags >nul 2>&1
+move /Y win98-bios-v1 .\Flags >nul 2>&1
+move /Y win98-bios-v2 .\Flags >nul 2>&1
+move /Y Xash3D-FWGS-v1 .\Flags >nul 2>&1
+move /Y xenia-canary-emu-v1 .\Flags >nul 2>&1
+move /Y xenia-canary-emu-v2 .\Flags >nul 2>&1
+move /Y xenia-emu-v1 .\Flags >nul 2>&1
+move /Y xenia-emu-v2 .\Flags >nul 2>&1
+move /Y xenia-manager-emu-v1 .\Flags >nul 2>&1
+move /Y yabasanshiro-emu-v1 .\Flags >nul 2>&1
+move /Y ZP-v1 .\Flags >nul 2>&1
 exit
