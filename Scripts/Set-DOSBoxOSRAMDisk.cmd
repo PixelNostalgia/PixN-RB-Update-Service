9b7314a9205c23fcbbde06e6f75237af2fd67b91
Team Pixel Nostalgia <team.pixel.nostalgia@gmail.com>
2026-08-25 13:39:33 +0100

    Update Set-DOSBoxOSRAMDisk.cmd

--- Scripts/Set-DOSBoxOSRAMDisk.cmd
+++ Scripts/Set-DOSBoxOSRAMDisk.cmd
@@ -6,6 +6,8 @@ color %colorCode%
 echo.
 echo Setting options for the Windows 98 pack...
 echo.
+REM **********************************************************************************DOS*********************************************************************************************************
+
 ping -n 2 127.0.0.1 >nul
 
 setlocal
@@ -31,5 +33,33 @@ powershell.exe -NoProfile -NonInteractive -ExecutionPolicy Bypass -Command ^
     "}"
 
 endlocal
+REM *********************************************************************************Win98********************************************************************************************************
+
+ping -n 2 127.0.0.1 >nul
+
+setlocal
+
+REM Set variable for the file path (relative to the script's location)
+set "filePath=..\..\emulationstation\.emulationstation\es_settings.cfg"
+
+REM Execute Windows PowerShell in Bypass mode
+powershell.exe -NoProfile -NonInteractive -ExecutionPolicy Bypass -Command ^
+    "$path = '%filePath%'; " ^
+    "try { " ^
+    "    [xml]$xml = Get-Content -LiteralPath $path; " ^
+    "    if ($xml.config -eq $null) { throw 'Root <config> node not found' } " ^
+    "    @($xml.config.string | Where-Object { $_.name -eq 'win98.dosbox_pure_bootos_ramdisk' }) | ForEach-Object { [void]$xml.config.RemoveChild($_) }; " ^
+    "    $newNode = $xml.CreateElement('string'); " ^
+    "    [void]$newNode.SetAttribute('name','win98.dosbox_pure_bootos_ramdisk'); " ^
+    "    [void]$newNode.SetAttribute('value','true'); " ^
+    "    [void]$xml.config.AppendChild($newNode); " ^
+    "    $xml.Save($path); " ^
+    "} catch { " ^
+    "    Write-Host 'Error occurred:' $_.Exception.Message; " ^
+    "    exit 1; " ^
+    "}"
+
+endlocal
 REM *******************************************************************************************************************************************************************************************
+
 exit
