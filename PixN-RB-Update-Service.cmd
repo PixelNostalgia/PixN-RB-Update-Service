f42f5b1a405b65f27b66e29f0bfdb7d887675596
Rapster-UK <rapsters.tech.hangout@gmail.com>
2026-08-28 22:07:29 +0100

    Update PixN-RB-Update-Service.cmd

--- PixN-RB-Update-Service.cmd
+++ PixN-RB-Update-Service.cmd
@@ -2,6 +2,10 @@
 title PixN Update Service...
 setlocal
 
+pushd "%~dp0" >nul 2>&1 || goto BadPath
+if /i not "%CD:~-15%"=="\emulators\pixn" goto BadPath
+if not exist "..\..\RetroBat.exe" goto BadPath
+
 rem Version 2.0
 rem Text color code for Light Green is A
 set "colorCode=A"
@@ -12,7 +16,7 @@ SET TempVBSFile=%temp%\~tmpSendKeysTemp.vbs
 IF EXIST "%TempVBSFile%" DEL /F /Q "%TempVBSFile%"
 ECHO Set WshShell = WScript.CreateObject("WScript.Shell") >>"%TempVBSFile%"
 ECHO Wscript.Sleep 900                                    >>"%TempVBSFile%"
-ECHO WshShell.SendKeys "{F11}"                            >>"%TempVBSFile%
+ECHO WshShell.SendKeys "{F11}"                            >>"%TempVBSFile%"
 ECHO Wscript.Sleep 900                                    >>"%TempVBSFile%"
 
 CSCRIPT //nologo "%TempVBSFile%"
@@ -26,3 +30,11 @@ ping -n 2 127.0.0.1 > nul
 echo.
 start /wait PixN-RB-Update-Service-Payload.cmd
 exit
+
+:BadPath
+color 0E
+echo.
+echo Make sure this is being run from your RetroBat\emulators\pixn folder.
+echo.
+pause
+exit /b 1
